df00ea7ca4a1a16d92faa4a4497907485d08714c
isafronov_at <kiraaaakka@gmail.com>
2025-07-08 19:12:55 +0300

    feat: добавить кастомную обработку ошибок для REST API сотрудников добавлены NoSuchEmployeeExtension и EmployeeIncorrectData для обработки ошибок обновлён MyRestController: добавлены обработчики исключений и корректные ответы об ошибках добавлен maven-compiler-plugin в pom.xml

--- src/main/java/com/safronov/spring/rest/controller/MyRestController.java
+++ src/main/java/com/safronov/spring/rest/controller/MyRestController.java
@@ -1,8 +1,12 @@
 package com.safronov.spring.rest.controller;
 
 import com.safronov.spring.rest.entity.Employee;
+import com.safronov.spring.rest.exeption_handling.EmployeeIncorrectData;
+import com.safronov.spring.rest.exeption_handling.NoSuchEmployeeExtension;
 import com.safronov.spring.rest.secvice.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,7 +24,29 @@ public class MyRestController {
     }
 
     @GetMapping("employees/{id}")
-    public Employee getEmployeeById(@PathVariable("id") int id) {
-        return employeeService.getEmployeeById(id);
+    public Employee getEmployeeById(@PathVariable("id") Integer id) {
+
+        Employee employee = employeeService.getEmployeeById(id);
+
+        if (employee == null) {
+            throw new NoSuchEmployeeExtension(String.format("Работника с id = '%d' нет в БД", id));
+        }
+
+        return employee;
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<EmployeeIncorrectData> handleEmployeeIncorrectData(NoSuchEmployeeExtension e) {
+        EmployeeIncorrectData incorrectData = new EmployeeIncorrectData();
+        incorrectData.setInfo(e.getMessage());
+
+        return new ResponseEntity<>(incorrectData, HttpStatus.NOT_FOUND);
+    }
+    @ExceptionHandler
+    public ResponseEntity<EmployeeIncorrectData> handleEmployeeIncorrectData(Exception e) {
+        EmployeeIncorrectData incorrectData = new EmployeeIncorrectData();
+        incorrectData.setInfo(e.getMessage());
+
+        return new ResponseEntity<>(incorrectData, HttpStatus.BAD_REQUEST);
     }
 }
