20cfcdf34b1a492df17acd1564decf50742a9970
isafronov_at <kiraaaakka@gmail.com>
2025-07-11 20:17:02 +0300

    Добавлен endpoint для удаления сотрудника по id. Возвращается текстовый ответ с подтверждением удаления.

--- src/main/java/com/safronov/spring/rest/controller/MyRestController.java
+++ src/main/java/com/safronov/spring/rest/controller/MyRestController.java
@@ -4,6 +4,8 @@ import com.safronov.spring.rest.entity.Employee;
 import com.safronov.spring.rest.exeption_handling.NoSuchEmployeeExtension;
 import com.safronov.spring.rest.secvice.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -42,5 +44,22 @@ public class MyRestController {
     @PutMapping("/employees")
     public Employee updateEmployee(@RequestBody Employee employee) {
         return employeeService.saveEmployee(employee);
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public ResponseEntity<String> deleteEmployee(@PathVariable(value = "id") Integer id) {
+
+        Employee employee = employeeService.getEmployeeById(id);
+
+        if (employee == null) {
+            throw new NoSuchEmployeeExtension("Работника с ID = " + id + " нет в БД");
+        }
+
+        employeeService.deleteEmployee(id);
+
+        return ResponseEntity
+            .ok()
+            .contentType(MediaType.valueOf("text/plain; charset=UTF-8"))
+            .body("Сотрудник с id = " + id + " был удалён!");
     }
 }
